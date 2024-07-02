813cb7998db2ce3cb59971144ba8f9d5e1a0fc98
sarascurcio <162715146+sarascurcio@users.noreply.github.com>
2024-07-02 09:37:37 -0300

    Add files via upload

---
+++ Botaofujao-master/js/index.js
@@ -0,0 +1,18 @@
+//alert para o botao sim
+
+function sim() {
+    alert("Se mata");
+    
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
